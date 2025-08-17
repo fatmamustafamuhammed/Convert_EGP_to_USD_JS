1d09d7d74ca88391d39b48cc655557ff46617df6
fatmamustafamuhammed <fmomen738@gmail.com>
2025-08-17 17:58:24 +0300

    Enhance script code

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let pound = document.getElementById("pound");
 let dollar = document.getElementById("dollar");
 
 pound.onkeyup = function () {
-    dollar.value = (pound.value / 48.51).toFixed(2);
+    dollar.value = (pound.value / 48.51);
 }
 
 dollar.onkeyup = function () {
-    pound.value = (dollar.value * 48.51).toFixed(2);
+    pound.value = (dollar.value * 48.51);
 }
